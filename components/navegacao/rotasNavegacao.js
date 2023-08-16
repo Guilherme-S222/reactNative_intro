fffef11e160336527313e466639da263412c6324
Guilherme-S222 <guilherme.anjolette@etec.sp.gov.br>
2023-08-15 22:44:55 -0300

    aula 15-08-2023

---
+++ components/navegacao/rotasNavegacao.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Home from './home';
+import Velha from '../velha/index'
